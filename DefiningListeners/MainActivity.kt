3bba10c5415e96744693f8aeb50d91dbacab59a6
Glendon Philipp Baculio <glendonphilippbaculio@gmail.com>
2023-04-15 19:04:05 +0800

    Defining Listeners

---
+++ DefiningListeners/MainActivity.kt
@@ -0,0 +1,17 @@
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val saveDataButton = findViewById<Button>(R.id.saveDataButton)
+
+        saveDataButton.setOnClickListener {
+            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
